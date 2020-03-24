36eb19231eef99d891aaa752f85e1430090c9ac0
Romeo Rizzi <romeo.rizzi@univr.it>
2020-03-24 07:10:59 +0100

    sistema la gerazione per for2

--- for2_std/gen/generatore.py
+++ for2_std/gen/generatore.py
@@ -4,37 +4,23 @@ from limiti import *
 from varie import *
 from sys import argv, exit, stderr
 import os
-from numpy.random import random, randint, seed as nseed
-from random import choice, sample, shuffle, seed as rseed
 
 usage="""Generatore di "prova".
 
 Parametri:
 * N (numero)
-* S (seed)
 
 Constraint:
-* 1 <= N <= %d
+* 0 <= N <= %d
 """ % (MAXN)
 
 
-def run(N):
-    print N
-
-
 if __name__ == "__main__":
-    if len(argv) != 3:
+    if len(argv) != 2:
         print usage
         exit(1)
 
-    N, S = map(int, argv[1:])
-    assert (1 <= N <= MAXN)
-
-    # su seed non positivo copia un input di esempio dal .tex
-    if S <= 0:
-        print extract_input()[-S],
-        exit(0)
-    nseed(S)
-    rseed(S)
-    run(N)
+    N = int(argv[1])
+    assert (0 <= N <= MAXN)
+    print N
 
