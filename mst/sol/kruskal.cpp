061bc12ea1c25eca37dbc8f421ab7cbcb8c9815b
Romeo Rizzi <romeo.rizzi@univr.it>
2020-05-23 11:29:48 +0200

    [mst] General task refactor, remove grader

--- mst/sol/kruskal.cpp
+++ mst/sol/kruskal.cpp
@@ -1,6 +1,53 @@
+#include <cstdio>
+#include <cassert>
+#include <cstdlib>
+
+static FILE *fr, *fw;
+
+// Declaring variables
+static int N;
+static int M;
+static int* A;
+static int* B;
+static int* P;
+static int* C;
+static int* D;
+static int minimo;
+
+// Declaring functions
+int mst(int N, int M, int* A, int* B, int* P, int* C, int* D);
+
+int main() {
+	fr = stdin;
+	fw = stdout;
+
+	// Reading input
+	fscanf(fr, " %d %d", &N, &M);
+	A = (int*)malloc((M) * sizeof(int));
+	B = (int*)malloc((M) * sizeof(int));
+	P = (int*)malloc((M) * sizeof(int));
+	for (int i0 = 0; i0 < M; i0++) {
+		fscanf(fr, " %d %d %d", &A[i0], &B[i0], &P[i0]);
+	}
+
+	// Calling functions
+	C = (int*)malloc((N-1) * sizeof(int));
+	D = (int*)malloc((N-1) * sizeof(int));
+	minimo = mst(N, M, A, B, P, C, D);
+
+	// Writing output
+	fprintf(fw, "%d\n", minimo);
+	for (int i0 = 0; i0 < N-1; i0++) {
+		fprintf(fw, "%d %d\n", C[i0], D[i0]);
+	}
+
+	fclose(fr);
+	fclose(fw);
+	return 0;
+}
 /*
  * Alice Cortinovis
- * 
+ *
  * MST con algoritmo di Kruskal
  */
 
@@ -26,7 +73,7 @@ vector< pair<int, long long int> > G[MAXN];
 archi ARCHI[MAXM];
 long long sol;
 vector<pair<int, int> > archi_sol;
-int N, M;
+// int N, M;
 int p[MAXN];
 
 bool comp(archi x, archi y) {
@@ -66,11 +113,11 @@ int mst(int N, int M, int A[], int B[], int P[], int C[], int D[]) {
             archi_sol.push_back(make_pair(ARCHI[i].a, ARCHI[i].b));
         }
     }
-    
+
     for (int i=0; i<(int)archi_sol.size(); i++) {
         C[i] = archi_sol[i].first;
         D[i] = archi_sol[i].second;
     }
-        
+
     return sol;
 }
