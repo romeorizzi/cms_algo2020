f0ff378285391da83f86ab030dfc8c007f2f16bd
Federico Picchi <federico.picchi@studenti.univr.it>
2020-05-23 15:22:03 +0200

    Modificato generatore Collage. Adesso bisogna specificare anche la lunghezza della sequenza "compressa". Rimossa modalità '3' in quanto non più necessaria.

--- collage/gen/generatore.cpp
+++ collage/gen/generatore.cpp
@@ -2,16 +2,15 @@
 Generatore di input per il problema Collage.
 
 arg1 --> lunghezza della sequenza numerica da generare
-arg2 --> numero di colori disponibili (1 - 256), non il valore massimo (0 - 255)
-arg3 --> modalita'
-arg4 --> seed
+arg2 --> lunghezza della sequenza numerica compressa
+arg3 --> numero di colori disponibili (3 - 256), non il valore massimo (2 - 255)
+arg4 --> modalita'
+arg5 --> seed
 
 VALORI ACCETTABILI PER MODALITA':
 - 0 parentesi
 - 1 chi2
 - 2 completamente random
-- 3 con tanti numeri ripetuti
-
 
 USI SPECIALI:
 arg1 < 0: i casi di esempio
@@ -20,42 +19,54 @@ arg1 < 0: i casi di esempio
 #include<cassert>
 #include<cstdlib>
 #include<random>
-#include<set>
+#include<unordered_set>
 #include<vector>
-
-using namespace std;
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
 
 #define MAX_LEN 1000
+#define OFFSET 2002
 #define MAX_COLOR_PALETTE 256
-#define N_MOD 4
+#define N_MOD 3
 #define LOG2(X) ((unsigned) (8*sizeof (unsigned long long) - __builtin_clzll((X)) - 1))
 
+namespace std {
+    /*un po' una schifezza, ma per il suo scopo limitato va bene.
+    UniqueID serve per il padding.
+    I nodi verranno inseriti in un order statistics tree per avere insertion piu' veloce rispetto ad un array, e access migliore rispetto ad una lista.*/
+    uint64_t uniqueIDCounter = OFFSET;
+    class NODO {
+        public:
+        NODO(int _colore, uint64_t id) : colore(_colore), uniqueID(id) {};
+        NODO(int _colore) : colore(_colore) { uniqueID = uniqueIDCounter; uniqueIDCounter += OFFSET;};
+        int colore;
+        vector<NODO> figli;
+        bool isDouble = false;
+        uint64_t uniqueID;
+    };
+    template<> struct less<std::NODO> {
+        bool operator() (const NODO& lhs, const NODO& rhs) const
+        {
+            return lhs.uniqueID < rhs.uniqueID;
+        }
+    };
+}
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<
+            NODO,
+            null_type,
+            less<NODO>,
+            rb_tree_tag,
+            tree_order_statistics_node_update > STATISTIC_TREE;
 
-/*un po' una schifezza, ma per il suo scopo limitato va bene*/
-class NODO {
-    public:
-#ifdef DEBUGME
-    NODO(int _colore, int id = 0) : colore(_colore), uniqueID(id) {};
-#else
-    NODO(int _colore, int id = 0) : colore(_colore) {};
-#endif
-    int colore;
-    vector<NODO> figli;
-    bool isDouble = false;
-#ifdef DEBUGME
-    int uniqueID;
-#endif
-};
-
-#ifdef DEBUGME
-int uniqueIDCounter = 123;
-#endif
+STATISTIC_TREE generatedTree;
 /*
 Divide il nodo in bucket in base alla sua lunghezza. Ogni bucket sara' un figlio e rappresentera' una "parentesi".
 Ogni bucket viene ridiviso ricorsivamente.
 I colori gia' usati hanno una maggior possibilita' di essere usati di nuovo per aumentare il grado di difficolta'.
 */
-void recursiveCall(NODO& node, int len, set<int>& usedColors, int& numColors) {
+void recursiveCall(NODO& node, int len, unordered_set<int>& usedColors, int& numColors) {
 #ifdef DEBUGME
     cout << "LEN:" << len << " ID:" << node.uniqueID << endl;
 #endif
@@ -64,6 +75,7 @@ void recursiveCall(NODO& node, int len, set<int>& usedColors, int& numColors) {
     if(len == 1) {
         return;
     } else if(len == 2) {
+        assert(len != 2 && "LEN 2 DOVREBBE ESSERE DISABILITATO");
         node.isDouble = true;
         return;
     } else {
@@ -76,27 +88,29 @@ void recursiveCall(NODO& node, int len, set<int>& usedColors, int& numColors) {
         int maxBucketSize = max(1, len / ((rand() % logC) + 1));
         int newColor, bucketSize;
         
+        //Colore da NON usare per il prossimo figlio
+        auto forbiddenColor = node.colore;
+
         //Continuo a suddividere finche' ho ancora spazio a disposizione.
         while(len > 0) {
-            newColor = rand() % (numColors * 3);
             bucketSize = min(len, 1 + (rand() % maxBucketSize));
-            if(newColor >= numColors) {
-                newColor = newColor % usedColors.size();
-                auto it = usedColors.begin();
-                advance(it, newColor);
-                newColor = *it;
+            if(bucketSize == 2) {
+                bucketSize = 1; //len 2 disabilitato (produrrebbe una sequenza non del tutto compressa)
             }
-            if(newColor == node.colore) {
-                newColor = (newColor + 1) % numColors;
-            }
+            do {
+                newColor = rand() % (numColors * 3);
+                if(newColor >= numColors) {
+                    newColor = newColor % usedColors.size();
+                    auto it = usedColors.begin();
+                    advance(it, newColor);
+                    newColor = *it;
+                }
+            } while(newColor == forbiddenColor || newColor == node.colore);
             usedColors.insert(newColor);
+            forbiddenColor = newColor;
             
-            #ifdef DEBUGME            
-            node.figli.push_back(NODO(newColor, uniqueIDCounter++));
-            #else
             //Aggiungo un nuovo figlio
             node.figli.push_back(NODO(newColor));
-            #endif
             
             //Suddivido anche il figlio
             recursiveCall(*node.figli.rbegin(), bucketSize, usedColors, numColors);
@@ -104,7 +118,7 @@ void recursiveCall(NODO& node, int len, set<int>& usedColors, int& numColors) {
 
 
             #ifdef DEBUGME 
-            if(node.uniqueID == 0) cout << "LEN:" << len << " ID:" << node.uniqueID << " B:" << bucketSize << " M:" << maxBucketSize << endl;
+            if(node.uniqueID == OFFSET) cout << "LEN:" << len << " ID:" << node.uniqueID << " B:" << bucketSize << " M:" << maxBucketSize << endl;
             #endif
         }
     }
@@ -116,7 +130,7 @@ Vedi --> treeMode
 */
 void visitPrint(NODO& node, bool ignoreMe = false) {
     if(!ignoreMe) {
-        cout << node.colore << " ";
+        generatedTree.insert(NODO(node.colore));
     }
     if(!node.figli.empty()) {
         for(size_t i = 0; i < node.figli.size(); i++) {
@@ -124,7 +138,7 @@ void visitPrint(NODO& node, bool ignoreMe = false) {
         }
     }
     if(!ignoreMe && (node.isDouble || !node.figli.empty())) {
-        cout << node.colore << " ";
+        generatedTree.insert(NODO(node.colore));
     }
 }
 
@@ -138,8 +152,7 @@ ma molto piu' probabilmente sara':
 */
 void treeMode(int& len, int& numColors, int& seed){
     
-    auto usedColors = set<int>();
-    srand(seed);
+    auto usedColors = unordered_set<int>();
     NODO radice (rand() % numColors);
     usedColors.insert(radice.colore);
 
@@ -159,18 +172,20 @@ void chiMode(int& len, int& numColors, int& seed){
     auto generator = default_random_engine(seed);
     int logC = LOG2(numColors) + 1;
     assert(logC > 0 && logC <= numColors);
-    chi_squared_distribution<double> distribution((logC + 1) / 2);
+    chi_squared_distribution<double> distribution(logC + 1);
 
     int count = 0;
+    int oldColor = numColors;
     int tmp;
-    while(count < len - 1) {
+    while(count < len) {
         tmp = distribution(generator);
         if(tmp >= 0 && tmp < numColors) {
+            tmp = (tmp + (tmp == oldColor)) % numColors;
+            oldColor = tmp;
             count++;
-            cout << tmp << " ";
+            generatedTree.insert(NODO(tmp));
         }
     }
-    cout << (int)distribution(generator) % numColors << endl;
 }
 
 
@@ -181,38 +196,52 @@ void chiMode(int& len, int& numColors, int& seed){
 Generazione molto semplice: col classico rand() % colori disponibili.
 */
 void randomMode(int& len, int& numColors, int& seed){
-    srand(seed);
-
-    for(int i = 0; i < len - 1; i++) {
-        cout << rand() % numColors << " ";
+    int oldColor = numColors;
+    int tmp;
+    for(int i = 0; i < len; i++) {
+        tmp = rand() % numColors;
+        tmp = (tmp + (tmp == oldColor)) % numColors;
+        oldColor = tmp;
+        generatedTree.insert(NODO(tmp));
     }
-    cout << rand() % numColors << endl;
 }
 
-/* CODICE DI RIFERIMENTO ==> 3
-Generazione in cui i primi ca. (log(numColors) + 1) colori hanno una grossa probabilita' di uscire. Piu' un ulteriore probabilita' che esca il colore 0.
+
+/*
+Duplica nodi finche' non raggiunge la giusta lunghezza.
+Per ogni iterazione, viene scelto un nodo e ne viene aggiunto uno uguale accanto
 */
-void repetitionMode(int& len, int& numColors, int& seed){
-    srand(seed);
-
-    int logC = LOG2(numColors) + 1;
-    assert(logC > 0 && logC <= numColors);
-
-    int modN = 10 * numColors;
-    
-    int temp;
-    for(int i = 0; i < len - 1; i++) {
-        temp = rand() % modN;
-        if(temp > numColors) {
-            cout << (logC & 1 ? 0 : temp % logC) << " ";
+void pad(size_t newSize) {
+    auto currentSize = generatedTree.size();
+    while(currentSize < newSize) {
+        size_t randomPos = rand() % currentSize++;
+        /*
+        auto elem = generatedTree.find_by_order(randomPos++);
+        auto n = generatedTree.find_by_order(randomPos++);
+        if(n!=generatedTree.end() && (n->colore == elem->colore)) {
+            do {
+                n = generatedTree.find_by_order(randomPos++);
+            } while(n!=generatedTree.end() && (n->colore == elem->colore));
+            n = generatedTree.find_by_order(randomPos - 2);
         } else {
-            cout << temp % numColors << " ";
+            n = elem;
         }
+        */
+        auto elem = generatedTree.find_by_order(randomPos);
+        auto n = elem;
+        NODO fakeNODE = *elem;
+        fakeNODE.uniqueID+= MAX_LEN + 1; //al piu' avro' duplicato quel nodo MAL_LEN volte
+        auto sameGroupPos = generatedTree.order_of_key(fakeNODE) - 1; //Trovo la posizione dell'ultimo nodo di quel tipo duplicato chiedendo all'albero "Quanti nodi ci sono ID minore di...?" e sottranedo 1 perche' le posizioni iniziano da 0.
+        if(randomPos != sameGroupPos && sameGroupPos != currentSize - 1) { //la seconda condizione non dovrebbe mai verificarsi. E' lì tanto per essere sicuri. Non cambia niente dal punto di vista della performance
+            n = generatedTree.find_by_order(sameGroupPos);
+        }
+
+#ifdef DEBUGME
+        cout << "elemColore: "<< elem->colore << " ID: " << elem->uniqueID << ". Aggiungo colore: " << n->colore << " ID: " << n->uniqueID + 1 << endl;
+#endif
+        generatedTree.insert(NODO(n->colore,n->uniqueID + 1));
     }
-    cout << rand() % logC << endl;    
 }
-
-
 
 int main(int argc, char** argv) {
     assert(argc > 0);
@@ -235,36 +264,57 @@ int main(int argc, char** argv) {
         }
         return 0;
     }
-    assert(argc == 5);
+    assert(argc == 6);
 
     int len = atoi(argv[1]);
     assert(len <= MAX_LEN && len > 0);
 
-    int numColors = atoi(argv[2]);
-    assert(numColors <= MAX_COLOR_PALETTE && numColors > 0);
+    int compressedLen = atoi(argv[2]);
+    assert(compressedLen <= len && compressedLen > 0);
 
-    int mod = atoi(argv[3]);
+    int numColors = atoi(argv[3]);
+    assert(numColors <= MAX_COLOR_PALETTE && numColors > 2);
+
+    int mod = atoi(argv[4]);
     assert(mod < N_MOD && mod >= 0);
 
-    int seed = atoi(argv[4]);
+    int seed = atoi(argv[5]);
+    srand(seed);
 
+    //Prima riga: lunghezza della sequenza
     cout << len << endl;
+
+    //Generiamo una sequenza compressa di lunghezza compressedLen
     switch(mod) {
         case 0:
-            treeMode(len, numColors, seed);
+            treeMode(compressedLen, numColors, seed);
             break;
         case 1:
-            chiMode(len, numColors, seed);
+            chiMode(compressedLen, numColors, seed);
             break;
         case 2:
-            randomMode(len, numColors, seed);
-            break;
-        case 3:
-            repetitionMode(len, numColors, seed);
+            randomMode(compressedLen, numColors, seed);
             break;
         default:
             return -1;
     }
+
+#ifdef DEBUGME
+    cout << "TREE PRIMA DI PADDING" << endl;
+    for(auto it = generatedTree.begin(); it != generatedTree.end(); it++) {
+        cout << it->colore << " ";
+    }
+    cout << endl << "Size: " << generatedTree.size() << endl << "TREE DOPO PADDING" << endl;
+#endif
+
+    //Padding della sequenza fino a raggiungere la lunghezza len
+    pad(len);
+
+    //Seconda ed ultima riga: la sequenza
+    for(auto it = generatedTree.begin(); it != generatedTree.end(); it++) {
+        cout << it->colore << " ";
+    }
+    cout << endl;
 
     return 0;
 }
