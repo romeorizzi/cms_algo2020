de0ab2d4ddc8dd0a65cc6314cc37b7e6e9a39585
Romeo Rizzi <romeo.rizzi@univr.it>
2022-03-26 11:23:36 +0100

    corretto il correttore per pillole_list

--- pillole_list/check/checker.cpp
+++ pillole_list/check/checker.cpp
@@ -5,9 +5,7 @@
 #include <set>
 #include <string>
 
-void
-ex(double res, const char *fmt, ...)
-{
+void ex(double res, const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
 
@@ -18,11 +16,9 @@ ex(double res, const char *fmt, ...)
     exit(0);
 }
 
-int
-main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     FILE *in, *cor, *out;
-    int N, i, j, correct_S, solution_S, I, M;
+    int N, correct_S, solution_S;
     char line[1024];
     std::set<std::string> values;
 
@@ -35,21 +31,21 @@ main(int argc, char *argv[])
     assert(fscanf(cor, "%d", &correct_S) == 1);
 
     if (fgets(line, sizeof(line), out) == NULL)
-        ex(0.0, "Errore di lettura della riga %d: probabile fine prematura del file", i);
+        ex(0.0, "Errore di lettura della prima riga del file soluzione generata dal codice dello studente: probabile fine prematura del file");
 
     if (sscanf(line, "%d", &solution_S) != 1)
-        ex(0.0, "Errore parsing del file soluzione alla riga 1: atteso un intero");
+        ex(0.0, "Errore parsing del file soluzione alla prima riga: atteso un intero");
 
     if (correct_S != solution_S)
         ex(0.0, "Numero di soluzioni errato: atteso %d, ottenuto %d", correct_S, solution_S);
 
-    for (i = 1; i <= correct_S; i++) {
+    for (int i = 1; i <= correct_S; i++) {
         if (fgets(line, sizeof(line), out) == NULL)
             ex(0.0, "Errore di lettura della riga %d: probabile fine prematura del file", i);
 
-        I = N;
-        M = 0;
-        for (j = 0; j < N * 2; j++) {
+        int I = N;
+        int M = 0;
+        for (int j = 0; j < N * 2; j++) {
             switch (line[j]) {
             case 'I':
                 if (I <= 0)
@@ -63,15 +59,14 @@ main(int argc, char *argv[])
                 M--;
                 break;
             default:
-                ex(0.0, "Carattere '%c' non valido alla riga %d carattere %d: possibile fine prematura della riga", line[j], i, j);
+                ex(0.0, "Carattere '%c' non valido nella posizione %d della riga %d: possibile fine prematura della riga", line[j], j, i);
             }
         }
-
         if (M != 0 || I != 0)
             ex(0.0, "Non hai consumato tutte le pillole alla riga %d", i);
 
         if (!values.insert(std::string(line)).second)
-            ex(0.0, "Riga %d ripetuta", i);
+            ex(0.0, "La riga %d ripete una riga precedente", i);
     }
 
     ex(1.0, "Corretto");
